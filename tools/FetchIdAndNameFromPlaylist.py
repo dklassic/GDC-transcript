d435dc2c4fe860fe15d5264f37fa2d7245dc219f
DK Liao <dklassic@gmail.com>
2023-04-11 21:46:10 +0800

    Updated tool scripts

---
+++ tools/FetchIdAndNameFromPlaylist.py
@@ -0,0 +1,53 @@
+import csv
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# Your API key here. Replace with your own Google API key.
+API_KEY = 'AIzaSyAVUOok0hhTmk1cLPiYHDVEwu1EeDIq6I4'
+
+def get_playlist_videos(playlist_id):
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+    # Get the list of videos in the playlist
+    videos = []
+    next_page_token = None
+
+    while True:
+        try:
+            response = youtube.playlistItems().list(
+                part='snippet',
+                playlistId=playlist_id,
+                maxResults=50,
+                pageToken=next_page_token
+            ).execute()
+
+            for item in response['items']:
+                video_id = item['snippet']['resourceId']['videoId']
+                video_title = item['snippet']['title']
+                videos.append((video_title, video_id))
+
+            next_page_token = response.get('nextPageToken')
+            if next_page_token is None:
+                break
+        except HttpError as e:
+            print(f"An HTTP error {e.resp.status} occurred: {e.content}")
+            break
+
+    return videos
+
+def save_to_csv(videos, output_file):
+    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Title', 'Video ID'])
+        writer.writerows(videos)
+
+def main():
+    playlist_id = 'PLNCOK8QkQUcr3TcwGyIesPZ4vRKrD5e72'  # Replace with the desired playlist ID
+    output_file = 'playlist_videos.csv'
+
+    videos = get_playlist_videos(playlist_id)
+    save_to_csv(videos, output_file)
+    print(f"Playlist video data saved to {output_file}")
+
+if __name__ == '__main__':
+    main()
--- tools/downloadAudio.py
+++ tools/downloadAudio.py
@@ -1,37 +1,43 @@
 import os
-import subprocess
+from pytube import YouTube
+from moviepy.editor import *
+from concurrent.futures import ThreadPoolExecutor
 
 def read_video_ids(filename):
     with open(filename, "r") as file:
         video_ids = file.readlines()
     return [video_id.strip() for video_id in video_ids]
 
-def download_audio(video_ids, output_folder):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+def download_audio(video_id, output_folder):
+    link = f"https://www.youtube.com/watch?v={video_id}"
+    print(f"Downloading audio from {link}...")
 
-    for video_id in video_ids:
-        link = f"https://www.youtube.com/watch?v={video_id}"
-        print(f"Downloading audio from {link}...")
-        try:
-            subprocess.check_output([
-                "youtube-dl",
-                "-f", "bestaudio/best",
-                "--extract-audio",
-                "--audio-format", "mp3",
-                "--output", f"{output_folder}/{video_id}.%(ext)s",
-                link
-            ])
-            print(f"Audio downloaded from {link}")
-        except subprocess.CalledProcessError as e:
-            print(f"Error downloading audio from {link}: {e}")
+    try:
+        yt = YouTube(link)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        output_file = audio_stream.download(output_path=output_folder, filename=video_id)
+
+        # Convert the downloaded file to MP3
+        input_audio = AudioFileClip(output_file)
+        input_audio.write_audiofile(os.path.join(output_folder, f"{video_id}.mp3"))
+        os.remove(output_file)  # Remove the original downloaded file
+
+        print(f"Audio downloaded from {link}")
+    except Exception as e:
+        print(f"Error downloading audio from {link}: {e}")
 
 def main():
     input_filename = "video_to_download.txt"
     output_folder = "gdc_audio"
 
     video_ids = read_video_ids(input_filename)
-    download_audio(video_ids, output_folder)
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Run the download process concurrently using multiple threads
+    with ThreadPoolExecutor() as executor:
+        executor.map(lambda video_id: download_audio(video_id, output_folder), video_ids)
 
 if __name__ == "__main__":
     main()
