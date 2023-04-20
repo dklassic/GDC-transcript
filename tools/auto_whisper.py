b580afca5e930fb893bb18d6374f9ac8423145aa
DK Liao <dklassic@gmail.com>
2023-04-20 17:54:38 +0800

    Change to yt_dlp

--- tools/auto_whisper.py
+++ tools/auto_whisper.py
@@ -1,19 +1,33 @@
 import os
 import sys
 import openai
-from pytube import YouTube
+# from pytube import YouTube
+from yt_dlp import YoutubeDL
 
 # not sure how to do this properly yet: https://platform.openai.com/docs/guides/speech-to-text/quickstart
 
 def download_video(video_id):
-    try:
-        video_url = f'http://youtube.com/watch?v={video_id}'
-        yt = YouTube(video_url)
-        audio_stream = yt.streams.filter(only_audio=True).first()
-        audio_stream.download(filename="video.mp3", output_path=".", skip_existing=False)
-        print(f"Audio downloaded from {video_url}")
-    except Exception as e:
-        print(f"Error downloading audio from {video_url}: {e}")
+    video_url = f'http://youtube.com/watch?v={video_id}'
+    # try:
+    #     yt = YouTube(video_url)
+    #     audio_stream = yt.streams.filter(only_audio=True).first()
+    #     audio_stream.download(filename="video.mp3", output_path=".", skip_existing=False)
+    #     print(f"Audio downloaded from {video_url}")
+    # except Exception as e:
+    #     print(f"Error downloading audio from {video_url}: {e}")
+    URLS = [video_url]
+    ydl_opts = {
+        "outtmpl": '/video.%(ext)s',
+        'format': 'm4a/bestaudio/best',
+        # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }]
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        error_code = ydl.download(URLS)
 
 
 def transcribe_audio():
