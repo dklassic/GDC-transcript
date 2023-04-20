02d7c7183485ab21db17abda418301c8f1feb7ea
DK Liao <dklassic@gmail.com>
2023-04-20 12:49:21 +0800

    Updated workflow

--- tools/auto_whisper.py
+++ tools/auto_whisper.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import re
+import subprocess
 from pytube import YouTube
 import requests
 
@@ -8,7 +10,7 @@ def download_video(video_id):
     video_url = 'http://youtube.com/watch?v=' + video_id
     yt = YouTube(video_url)
     audio_stream = yt.streams.filter(only_audio=True).first()
-    audio_stream.download(filename="video", output_path=".", skip_existing=False)
+    audio_stream.download(filename="video.mp3", output_path=".", skip_existing=False)
 
 def transcribe_audio():
     headers = {
@@ -21,8 +23,12 @@ def transcribe_audio():
     data = json.loads(response.text)
     return data['text']
 
+def convert_audio_to_wav(input_file, output_file):
+    subprocess.run(["ffmpeg", "-i", input_file, output_file])
+
 def main(video_id):
     download_video(video_id)
+    convert_audio_to_wav("video.mp3", "video.wav")
     transcript = transcribe_audio()
 
     with open(f"static/src/subtitle/{video_id}.srt", 'w', encoding='utf-8') as f:
